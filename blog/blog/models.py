0854b40df00ac994de42b4651ae041a83e4609ef
mgh35 <markgeorgehiggins@hotmail.com>
2021-02-06 09:37:55 -0500

    Add Post title

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 class Post(models.Model):
     create_time = models.DateTimeField(auto_now_add=True, blank=True)
+    title = models.CharField(max_length=256, default="No Title")
     text = models.TextField()
