b27e1c940ad8c818336686a5b08cdd352776a82a
mgh35 <markgeorgehiggins@hotmail.com>
2021-02-06 09:30:24 -0500

    Show posts on homepage

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.template import loader
 
+from blog.models import Post
+
 
 def home(request):
     template = loader.get_template("home.html")
-    return HttpResponse(template.render({}, request))
+    context = {
+        "posts": Post.objects.order_by("-create_time")[:10]
+    }
+    return HttpResponse(template.render(context, request))
