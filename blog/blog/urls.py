3572fc49d7683b7f47bd2a946bb0bdfc9a53ebb1
mgh35 <markgeorgehiggins@hotmail.com>
2021-02-07 16:35:03 -0500

    Specify auth views explicitly

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from blog import views
@@ -7,7 +7,8 @@ from blog import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("", views.public_home, name="public-home"),
     path("home/", views.home, name="home"),
 ]
