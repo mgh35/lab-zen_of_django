51b0c2792889bb1c3a1bdff3b677ff76ea4ca477
mgh35 <markgeorgehiggins@hotmail.com>
2021-02-07 16:00:58 -0500

    Move templates into app

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -6,13 +6,13 @@ from blog.models import Post
 
 
 def home(request):
-    template = loader.get_template("home.html")
+    template = loader.get_template("blog/home.html")
     context = {"posts": Post.objects.order_by("-create_time")[:10]}
     return HttpResponse(template.render(context, request))
 
 
 @login_required
 def profile(request):
-    template = loader.get_template("profile.html")
+    template = loader.get_template("blog/profile.html")
     context = {}
     return HttpResponse(template.render(context, request))
