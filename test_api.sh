dcdb280646e8da9dfd7d813822e7271aea00b045
mgh35 <markgeorgehiggins@hotmail.com>
2021-02-11 21:31:22 -0500

    Update API calls

--- test_api.sh
+++ test_api.sh
@@ -1,3 +1,9 @@
-curl localhost:8000/posts/
+curl -H "Accept: application/json" localhost:8000/posts/
+curl -H "Accept: application/json" localhost:8000/posts/1/
 
-curl localhost:8000/posts/1/
+curl -X POST -H "Accept: application/json" -d @- localhost:8000/posts/ <<EOF
+{
+  "title": "My Grandiose Title",
+  "text": "Some really boring thing. Blah blah blah."
+}
+EOF
