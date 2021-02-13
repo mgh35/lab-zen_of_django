7cda5651e5faedfa4754dfd11f728c4927b43653
mgh35 <markgeorgehiggins@hotmail.com>
2021-02-13 10:48:08 -0500

    Add author field

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Post(models.Model):
     create_time = models.DateTimeField(auto_now_add=True, blank=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=256, default="No Title")
     text = models.TextField()
