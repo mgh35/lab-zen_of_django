98713f9325da4e249925f2a7066b7a58661d9d5b
mgh35 <markgeorgehiggins@hotmail.com>
2021-02-13 10:20:44 -0500

    Add exception handler template

--- blog/blog/renderers.py
+++ blog/blog/renderers.py
@@ -2,5 +2,7 @@ from rest_framework.renderers import TemplateHTMLRenderer
 
 
 class ModelTemplateHTMLRenderer(TemplateHTMLRenderer):
+    exception_template_names = ["blog/%(status_code)s.html"]
+
     def get_template_context(self, data, renderer_context):
         return {"data": data}
