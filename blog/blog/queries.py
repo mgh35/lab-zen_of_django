3bf074d45137b1ac25a0ccfd1c6e0657c916dbe5
mgh35 <markgeorgehiggins@hotmail.com>
2021-02-13 11:40:28 -0500

    Exclude other user's private posts

--- blog/blog/queries.py
+++ blog/blog/queries.py
@@ -1,8 +1,13 @@
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.db.models import QuerySet
 
 from blog.models import Post
 
 
 def all_permissioned_posts(user: User) -> QuerySet:
-    return Post.objects.all()
+    if user.is_authenticated:
+        user_filter = Q(is_public=True) | Q(author=user)
+    else:
+        user_filter = Q(is_public=True)
+    return Post.objects.filter(user_filter)
