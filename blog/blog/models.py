608f4959905c4c7da10d10ffc0a1829d3727c733
mgh35 <markgeorgehiggins@hotmail.com>
2021-02-14 16:22:10 -0500

    Add Comment

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -8,3 +8,10 @@ class Post(models.Model):
     title = models.CharField(max_length=256, default="No Title")
     text = models.TextField()
     is_public = models.BooleanField(default=True)
+
+
+class Comment(models.Model):
+    create_time = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField()
